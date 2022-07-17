2b24465eb45bbbfa895e5ef2ffca34b4acaa91e8
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-16 20:10:03 -0600

    user story 1

--- spec/features/recipes/index_spec.rb
+++ spec/features/recipes/index_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes index' do
-  it "displays the recipes" do
+  it "displays the recipes user story 1" do
     recipe1 = Recipe.create(name: 'pepper chicken', complexity: 3, genre: 'Sci-Fi')
+    recipe2 = Recipe.create(name: 'banana bread', complexity: 4, genre: 'good')
 
     visit "/recipes"
     expect(current_path).to eq("/recipes")
@@ -10,5 +11,8 @@ RSpec.describe 'Recipes index' do
     expect(page).to have_content('pepper chicken')
     expect(page).to have_content(3)
     expect(page).to have_content('Sci-Fi')
+    expect(page).to have_content(recipe2.name)
+    expect(page).to have_content(recipe2.complexity)
+    expect(page).to have_content(recipe2.genre)
   end
 end
